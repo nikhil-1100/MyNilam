59efca6a9e17cb16968351a595bd6b501f2151dc
nikhil-1100 <optimalaces.2001@gmail.com>
2026-07-18 19:49:05 +0530

    chore: clean up deleted django backend files

--- apps/django-backend/properties/api_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api.views import PropertyListView, PropertyDetailView, PropertyPostAdsView
-urlpatterns = [
-    path('', PropertyListView.as_view(), name='property-list'),
-    path('<int:pk>/', PropertyDetailView.as_view(), name='property-detail'),
-    path('post-ads/',PropertyPostAdsView.as_view(),name='property-post-ads')
-]
